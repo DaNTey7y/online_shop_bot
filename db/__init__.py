16fd31bd30d147d94d5b2dcfd113e3a852963379
xvhuqdph <xvhuqdph@gmail.com>
2023-08-28 19:34:50 +0500

    - Изменения

--- db/__init__.py
+++ db/__init__.py
@@ -1,16 +1,18 @@
 from .base import Base
-from .models import User, Section, Product
+from .models import User, Section, Product, Operation
 from .requests import *
 
 
 __all__ = [
     "Base",
     "User",
-    "get_user_data",
     "Section",
     "Product",
+    "Operation",
+    "get_user_data",
     "get_sections",
     "get_goods_by_section",
     "get_section_data",
-    "get_product"
+    "get_product",
+    "get_user_history"
 ]
