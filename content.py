f37e10153f4e905b95fad739f4e2c5859a69d610
xvhuqdph <xvhuqdph@gmail.com>
2024-01-03 22:40:27 +0500

    - новые описания

--- content.py
+++ content.py
@@ -2,11 +2,18 @@ from aiogram.types import InputMediaPhoto, FSInputFile
 
 
 def show_user_info(user_id, orders, reg_date):
+    months = {1: "января", 2: "февраля", 3: "марта",
+              4: "апреля", 5: "мая", 6: "июня",
+              7: "июля", 8: "августа", 9: "сентября",
+              10: "октября", 11: "ноября", 12: "декабря"}
+
+    f_date = f"{reg_date.day} {months[reg_date.month]} {reg_date.year}"
+
     return InputMediaPhoto(
         media=FSInputFile("static/images/profile.png"),
         caption=f"Ваш ID профиля: {user_id}\n"
                 f"Кол-во ваших заказов: {orders}\n"
-                f"Дата регистрации: {reg_date}"
+                f"Дата регистрации: {f_date}"
     )
 
 
@@ -14,6 +21,15 @@ def show_section_info(img_path, img_caption):
     return InputMediaPhoto(
         media=FSInputFile(img_path),
         caption=img_caption
+    )
+
+
+def show_orders_history(orders_info):
+    f_message = "Ваши покупки:\n" + "\n".join(orders_info[::-1])
+
+    return InputMediaPhoto(
+        media=FSInputFile("static/images/profile.png"),
+        caption=f_message
     )
 
 
@@ -55,4 +71,9 @@ REVIEWS_DATA = InputMediaPhoto(
 
 SHOP_SECTION_2 = InputMediaPhoto(
     media=FSInputFile("static/images/oh.jpg")
+)
+
+SUPPORT_IMAGE = InputMediaPhoto(
+    media=FSInputFile("static/images/support.png"),
+    caption='Задайте свой вопрос модератору и получите на него ответ'
 )
